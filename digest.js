ff9e8749f04aac3f63fbdf4b4b142b950f87e765
OlgaKhinevich <olga-khinevich@yandex.ru>
2019-09-04 20:49:06 +0300

    scroll

---
+++ digest.js
@@ -0,0 +1,21 @@
+jQuery(function(f){
+    f(window).scroll(function(){
+        var element = f('#howitworks .animation-1, #howitworks .animation-2'); 
+        element['fade'+ (f(this).scrollTop() > 30 ? 'In': 'Out')](800); 
+        element = f('#howitworks .heading-text'); 
+        element['fade'+ (f(this).scrollTop() > 40 ? 'In': 'Out')](800); 
+        element = f('#howitworks .switches');  
+        element['fade'+ (f(this).scrollTop() > 100 ? 'In': 'Out')](800);  
+    });
+});
+
+
+
+
+
+
+
+
+
+
+  
